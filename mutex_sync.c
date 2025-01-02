30a4eefd90960d6b4cd37cc1073ef9d668821b57
wen2ms <1819509111@qq.com>
2025-01-02 21:34:01 +0800

    Feat: thread mutex

--- mutex_sync.c
+++ mutex_sync.c
@@ -6,8 +6,12 @@ const int kMax = 50;
 
 int number = 0;
 
+pthread_mutex_t mutex;
+
 void* counter_a(void* arg) {
     for (int i = 0; i < kMax; ++i) {
+        pthread_mutex_lock(&mutex);
+
         int cursor = number;
 
         ++cursor;
@@ -16,6 +20,8 @@ void* counter_a(void* arg) {
         number = cursor;
 
         printf("Thread A, id = %p, number = %d\n", pthread_self(), number);
+
+        pthread_mutex_unlock(&mutex);
     }
     
     return NULL;
@@ -23,12 +29,16 @@ void* counter_a(void* arg) {
 
 void* counter_b(void* arg) {
     for (int i = 0; i < kMax; ++i) {
+        pthread_mutex_lock(&mutex);
+
         int cursor = number;
-        
+
         number = ++cursor;
 
         printf("Thread B, id = %p, number = %d\n", pthread_self(), number);
-        
+
+        pthread_mutex_unlock(&mutex);
+
         usleep(5);
     }
 
@@ -37,12 +47,15 @@ void* counter_b(void* arg) {
 
 int main() {
     pthread_t thread_a, thread_b;
+    pthread_mutex_init(&mutex, NULL);
 
     pthread_create(&thread_a, NULL, counter_a, NULL);
     pthread_create(&thread_b, NULL, counter_b, NULL);
 
     pthread_join(thread_a, NULL);
     pthread_join(thread_b, NULL);
+
+    pthread_mutex_destroy(&mutex);
 
     return 0;
 }
